347ced630e5330c3fcfd463481eafd5e01fc04e4
praveenkris <praveen@polargold.de>
2021-09-30 17:07:47 +0200

    arrays basic

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        for(int i=0; i<array1.length;i++)
+            System.out.println(array1[i]);
+    }
+}
